e9c1069c3f0d4a488ec06f26186c5b27ea3e0d1d
Roger Sillito <rogersillito@gmail.com>
2023-04-25 16:34:48 +0100

    update test harness to work with relocated test files

--- tools/testharness.cmd
+++ tools/testharness.cmd
@@ -1,8 +1,9 @@
 @REM ASSUMES project checked out at C:\DEV\medialib
 @REM Also see postman
-@SET slash=%%5c
-@SET colon=%%3a
-@SET fpath=C%colon%%slash%DEV%slash%medialib%slash%tools%slash%testdata
+@SET slash=%%5C
+@SET colon=%%3A
+@REM relative path resolved from project root
+@SET fpath=src%slash%test%slash%resources%slash%testdata
 curl -o - -i -X PUT http://127.0.0.1:8080/api/v1/directory?path=%fpath%
-curl -o - -i -X GET http://127.0.0.1:8080/api/v1/directory?path=%fpath%
+curl -o - -i -X GET http://127.0.0.1:8080/api/v1/directory?path=%fpath%%slash%A
 curl -o - -i -X GET http://127.0.0.1:8080/api/v1/directory?path=%fpath%%slash%MISSING
